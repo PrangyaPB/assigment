7c3aa7b675e4ee7bc0bd60a178a30941d0b410cb
PrangyaPB <prangyaparamita78gps@gmail.com>
2025-03-30 18:02:26 +0530

    history

---
+++ biodata.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<<"NAME= Prangya Paramita Bihari";
+    cout<<"regd. no. = 2403020061";
+    cout<<"BRANCH= Chemical Engineering";
+    return 0;
+    
+}
